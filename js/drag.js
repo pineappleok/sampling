343b1f84c3261e46532972b19fc15c46ab9637c8
pineappleok <550185271@qq.com>
2019-07-30 19:16:42 +0800

    加注释，去掉空白行；

--- js/drag.js
+++ js/drag.js
@@ -1,5 +1,4 @@
 (function () {
-
     var dnd = {
       // 初始化
       init: function () {
@@ -7,27 +6,19 @@
         console.log(this);
         me.src = document.querySelector('#demo1-src');
         me.panelList = document.querySelector('.panel-list');
-
         // 为拖拽源监听dragstart,设置关联数据
         me.src.addEventListener('dragstart', me.onDragStart, false);
-
         // 拖拽鼠标移入元素,在拖放目标上设置视觉反馈
         me.panelList.addEventListener('dragenter', me.onDragEnter, false);
-
         // 取消元素dragover默认行为,使其可拖放
         me.panelList.addEventListener('dragover', me.onDragOver, false);
-
         // 拖拽移出元素,清除视觉反馈
         me.panelList.addEventListener('dragleave', me.onDragLeave, false);
-
         // 鼠标释放,在拖放目标上接收数据并处理
         me.panelList.addEventListener('drop', me.onDrop, false);
-
         me.src.addEventListener('drag', function(e) {
         	console.log(e.target + 'onDrag');
         },false);
-
-
         me.src.addEventListener('dragend', function(e) {
         	console.log(e.target + 'onDragEnd');
         },false);
@@ -49,6 +40,7 @@
         }
       },
       onDragOver: function (e) {
+        //进入，就设置可以拖放进来（设置不执行默认：【默认的是不可以拖动进来】
       	console.log(e.target + ' onDragOver');
         e.preventDefault();
       },
@@ -62,8 +54,6 @@
           target.classList.remove('over');
         }
       }
-
     };
-
     dnd.init();
   }());
