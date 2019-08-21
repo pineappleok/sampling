88586734ea1228ab851b53d9387912e94dbc9637
pineappleok <550185271@qq.com>
2019-08-21 14:50:32 +0800

    可拖拽更新；

--- js/drag.js
+++ js/drag.js
@@ -1,59 +1,80 @@
-(function () {
-    var dnd = {
-      // 初始化
-      init: function () {
-        var me = this;
-        console.log(this);
-        me.src = document.querySelector('#demo1-src');
-        me.panelList = document.querySelector('.panel-list');
-        // 为拖拽源监听dragstart,设置关联数据
-        me.src.addEventListener('dragstart', me.onDragStart, false);
-        // 拖拽鼠标移入元素,在拖放目标上设置视觉反馈
-        me.panelList.addEventListener('dragenter', me.onDragEnter, false);
-        // 取消元素dragover默认行为,使其可拖放
-        me.panelList.addEventListener('dragover', me.onDragOver, false);
-        // 拖拽移出元素,清除视觉反馈
-        me.panelList.addEventListener('dragleave', me.onDragLeave, false);
-        // 鼠标释放,在拖放目标上接收数据并处理
-        me.panelList.addEventListener('drop', me.onDrop, false);
-        me.src.addEventListener('drag', function(e) {
-        	console.log(e.target + 'onDrag');
-        },false);
-        me.src.addEventListener('dragend', function(e) {
-        	console.log(e.target + 'onDragEnd');
-        },false);
-      },
-      onDragStart: function (e) {
-        e.dataTransfer.setData('Text', 'demo1-src');
-        console.log(e.target + ' onDragStart');
-      },
-      onDragEnter: function (e) {
-      	console.log(e.target + ' onDragEnter');
-        if (e.target.classList.contains('panel-item')) {
-          e.target.classList.add('over');
+//获取元素
+var draggleTitle = document.getElementById('draggleTitle');
+var dv = document.getElementById('dv');
+var x = 0;
+var y = 0;
+var l = 0;
+var t = 0;
+var isDown = false;
+//鼠标按下事件
+draggleTitle.onmousedown = function(e) {
+    //获取x坐标和y坐标
+    x = e.clientX;
+    y = e.clientY;
+
+    //获取左部和顶部的偏移量
+    l = dv.offsetLeft;
+    t = dv.offsetTop;
+    //开关打开
+    isDown = true;
+    //设置样式  
+    dv.style.cursor = 'move';
+    //鼠标移动
+    document.onmousemove = function(e) {
+        if (isDown == false) {
+            return;
         }
-      },
-      onDragLeave: function (e) {
-      	console.log(e.target + ' onDragLeave');
-        if (e.target.classList.contains('panel-item')) {
-          e.target.classList.remove('over');
+        //获取x和y
+        var nx = e.clientX;
+        var ny = e.clientY;
+        //计算移动后的左偏移量和顶部的偏移量
+        var nl = nx - (x - l);
+        var nt = ny - (y - t);
+        if(nl<=0)//左右边界限定
+        {
+            nl=0;
         }
-      },
-      onDragOver: function (e) {
-        //进入，就设置可以拖放进来（设置不执行默认：【默认的是不可以拖动进来】
-      	console.log(e.target + ' onDragOver');
-        e.preventDefault();
-      },
-      onDrop: function (e) {
-      	console.log(e.target + 'onDrop');
-        var id = e.dataTransfer.getData('Text');
-        var src = document.getElementById(id);
-        var target = e.target;
-        if (target.classList.contains('panel-item')) {
-          target.appendChild(src);
-          target.classList.remove('over');
+        else if(nl>=getInner().w-dv.offsetWidth)//元素在最右边时的判定，屏幕的宽度减去元素自身的宽度
+        {
+            nl=Math.floor(getInner().w-dv.offsetWidth);
         }
-      }
-    };
-    dnd.init();
-  }());
+        if(nt<=0)//上下边界限定
+        {
+            nt=0;
+        }
+        else if(nt>=getInner().h-dv.offsetHeight)//元素在最下边时的判定，屏幕的高度减去元素自身的高度
+        {
+            nt=Math.floor(getInner().h-dv.offsetHeight) - 1;
+        }
+        dv.style.left = nl + 'px';
+        dv.style.top = nt + 'px';
+    }
+    //鼠标抬起事件
+    document.onmouseup = function() {
+        //开关关闭
+        isDown = false;
+        dv.style.cursor = 'default';
+    }
+}
+//得到浏览器页面内容区窗口的大小
+function getInner()
+{
+	if(typeof innerHeight=='undefined')
+	{
+		return {
+			w:document.documentElement.clientWidth,
+			h:document.documentElement.clientHeight
+		}//IE浏览器适用
+	}
+	else
+	{
+		return {
+			w: innerWidth,
+			h: innerHeight
+		}//非IE浏览器适用
+	}
+}
+function hideOrShow(){
+    var childDialog = document.getElementById('childDialog');
+    childDialog.classList.toggle('show');
+}
